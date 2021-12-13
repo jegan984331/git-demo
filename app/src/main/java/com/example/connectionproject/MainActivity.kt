bdee6911f458cda7a8ba19fca4fc9e3e5c7c6766
jegan <jegan.s@protectt.ai>
2021-12-13 11:40:47 +0530

    init commit

---
+++ app/src/main/java/com/example/connectionproject/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.connectionproject
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.checkconnectionmodule.MainModule
+import com.example.checkconnectionmodule.Response
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        MainModule.initSDK(
+            applicationContext as Application,
+           object : MainModule.ScanNotify{
+                override fun onDetected(response: Response) {
+                    Log.i(TAG, "===========>>>> onDetected: code: ${response.code} --- isDetected: ${response.detected}")
+                }
+            }
+        )
+
+    }
+}
