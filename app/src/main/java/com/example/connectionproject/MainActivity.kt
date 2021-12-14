5549ea75146cd6a3322d1914c077765de269df1c
jegan <jegan.s@protectt.ai>
2021-12-14 10:18:59 +0530

    Toast was remove

--- app/src/main/java/com/example/connectionproject/MainActivity.kt
+++ app/src/main/java/com/example/connectionproject/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this,"Add msg",Toast.LENGTH_SHORT).show()
         MainModule.initSDK(
             applicationContext as Application,
            object : MainModule.ScanNotify{
